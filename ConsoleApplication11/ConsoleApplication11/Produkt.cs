d67df9f60afa4c5c4cc5c68db0b0dfee518716e8
gradampl <gr_adam@o2.pl>
2017-11-29 19:01:04 +0100

    Add files via upload

---
+++ ConsoleApplication11/ConsoleApplication11/Produkt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Produkt:ICloneable
+    {
+       public string nazwa;
+       public double cenaJednostkowa;
+       public double ilosc;
+
+        public Produkt()
+       {
+
+       }
+        public Produkt(string nazwa, double cenaJednostkowa, double ilosc)
+        {
+            this.nazwa = nazwa;
+            this.cenaJednostkowa = cenaJednostkowa;
+            this.ilosc = ilosc;
+        }
+        static void KopiujProdukt()
+        {
+           
+        }
+
+    }
+}
